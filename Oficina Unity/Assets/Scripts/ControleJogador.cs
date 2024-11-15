79804ddf50abe1e1804453d906f7b1d09ff8f4b7
IanNicolasMagattiDaSilva <157429018+IanNicolasMagattiDaSilva@users.noreply.github.com>
2024-11-14 22:27:17 -0300

    Atualização de animações
    Agora o personagem morre, adicionei mais tipos de moedas, adicionei animações para a morte, para o hit do player, e para todos os novos tipo de moedas

--- Oficina Unity/Assets/Scripts/ControleJogador.cs
+++ Oficina Unity/Assets/Scripts/ControleJogador.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ControleJogador : MonoBehaviour
 {
@@ -8,51 +10,109 @@ public class ControleJogador : MonoBehaviour
     public Controlador controlador;
     public Animator anim;
     float fator = 0;
+    public int vida;
     private float movimento;
 
+    
     private void Start()
     {
         anim = GetComponent<Animator>();
+        StartCoroutine("MoveSet");
     }
     // Update is called once per frame
     void Update()
     {
-        movimento = Input.GetAxis("Horizontal");
         
-        if (movimento != 0)
-        {
-            anim.SetBool("Move", true);
-            if(movimento > 0)
-            {
-                transform.eulerAngles = Vector3.zero;
-                fator = 1;
-            }
-            else
-            {
-                transform.eulerAngles = new Vector3(0f,180f,0f);
-                fator = -1;
-            }
-            transform.Translate(Vector2.right * movimento * velocidade * fator * Time.deltaTime);
-        }
-        else
-        {
-            anim.SetBool("Move", false);
-        }
 
     }
     
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Fruta")
+        if (other != null)
         {
-            controlador.EncostouFruta();
+            Animator animOther = other.gameObject.GetComponent<Animator>();
+            Cair cair = other.gameObject.GetComponent<Cair>();
+            Debug.Log(other.name);
+            cair.velocidade = 0;
+            if (other.gameObject.tag == "Fruta")
+            {
+                controlador.EncostouFruta();
+                animOther.SetBool("effect", true);
+                StartCoroutine(TimeToDestroy(0.25f, other));
+            }
+            else if (other.gameObject.tag == "Bomba")
+            {
+                vida -= 1;
+                if( vida == 0 )
+                {
+                    anim.SetBool("Dead", true);
+                    StartCoroutine(TimeToDestroy(0.5f, other));
+                    StopAllCoroutines();
+                }
+                else
+                {
+                    animOther.SetBool("explode", true);
+                    StartCoroutine("AnimHit");
+                    StartCoroutine(TimeToDestroy(0.5f, other));
+                }
+                
+            }
+        }
+
+    }
+
+
+    IEnumerator TimeToDestroy(float s, Collider2D other)
+    {
+        yield return new WaitForSeconds(s);
+        if(other != null)
+        {
             Destroy(other.gameObject);
         }
-        else if (other.gameObject.tag == "Bomba")
+        yield return null;
+    }
+
+    IEnumerator AnimHit()
+    {
+        anim.SetBool("Hit", true);
+        yield return new WaitForSeconds(1);
+        anim.SetBool("Hit", false);
+        yield return null;
+    }
+    IEnumerator AnimDie()
+    {
+        anim.SetBool("Hit", true);
+        yield return new WaitForSeconds(1);
+        anim.SetBool("Hit", false);
+        yield return null;
+    }
+    IEnumerator MoveSet()
+    {
+        while (true)
         {
-            controlador.EncostouBomba();
-            Destroy(other.gameObject);
+            movimento = Input.GetAxis("Horizontal");
+
+            if (movimento != 0)
+            {
+                anim.SetBool("Move", true);
+                if (movimento > 0)
+                {
+                    transform.eulerAngles = Vector3.zero;
+                    fator = 1;
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector3(0f, 180f, 0f);
+                    fator = -1;
+                }
+                transform.Translate(Vector2.right * movimento * velocidade * fator * Time.deltaTime);
+            }
+            else
+            {
+                anim.SetBool("Move", false);
+            }
+            yield return new WaitForEndOfFrame();
         }
     }
 }
--- Oficina Unity/Assets/Scripts/DestruirObjeto.cs
+++ Oficina Unity/Assets/Scripts/DestruirObjeto.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DestruirObjeto : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other.name);
-        if (other.gameObject.tag == "Fruta")
+        if (other != null)
         {
-            Destroy(other.gameObject);
-        }        
-        if (other.gameObject.tag == "Bomba")
-        {
-            Destroy(other.gameObject);
+            Animator anim = other.gameObject.GetComponent<Animator>();
+            Cair cair = other.gameObject.GetComponent<Cair>();
+            Debug.Log(other.name);
+            cair.velocidade = 0;
+            if (other.gameObject.tag == "Fruta")
+            {
+                anim.SetBool("explode", true);
+                StartCoroutine(TimeToDestroy(0.25f, other));
+            }
+            if (other.gameObject.tag == "Bomba")
+            {
+                anim.SetBool("explode", true);
+                StartCoroutine(TimeToDestroy(0.5f, other));
+            }
         }
+    }
+    IEnumerator TimeToDestroy(float s, Collider2D other)
+    {
+        yield return new WaitForSeconds(s);
+        Destroy(other.gameObject);
+        yield return null;
     }
 }
